c67069032ba0820365a076918b90e073cddedaef
jdi146 <jdi147.com@gamil.com>
2023-07-03 22:07:31 +0800

    refactor(/): web server

--- scripts/webui.sh
+++ scripts/webui.sh
@@ -3,10 +3,7 @@
 inbrowser=true
 
 python src/ui/webui.py \
-  --model_dir baichuan-inc/baichuan-7B \
   --quantization_type hf \
-  --bits 4 \
   --inbrowser inbrowser\
   --overwrite_cache \
   --port 7860 \
-  --stream_time_out 20
